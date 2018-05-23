7ab7bb5ba20a24334d3fa1d7e4311285248db927
Cat Bee <catherine.bodnar@gmail.com>
2018-05-22 21:37:22 -0700

    added bash script

---
+++ CatsWebsite2000/build.sh
@@ -0,0 +1,11 @@
+cat templates/top.html content/catbio.html templates/bottom.html > docs/catbio.html 
+
+
+
+cat templates/top.html content/catblog.html templates/bottom.html > docs/catblog.html
+
+
+
+cat templates/top.html content/catcontact.html templates/bottom.html > docs/catcontact.html
+
+
