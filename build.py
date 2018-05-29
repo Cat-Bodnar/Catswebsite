0931a976e8e341c518a14d8dc0ee8a91f7d0b4d7
Cat Bee <catherine.bodnar@gmail.com>
2018-05-28 18:37:51 -0700

    ran python script

--- build.py
+++ build.py
@@ -1,24 +1,26 @@
 
 
-def main(run_page):
+def main():
+    
     top = open('templates/top.html').read()
-    bottom = open('templates.bottom.html').read()
-    content = open('contents/index.html').read ()
-    index_html = top_template + content + bottom_template
-    open('contents/index.html','w+').write(index_html)
+    bottom = open('templates/bottom.html').read()
+    content = open('content/index.html').read ()
+    
+    index_html = top + content + bottom
+    open('content/index.html','w+').write(index_html)
 
-    content = open('contents/contact.html').read()
-    contact_html = top_template + content + bottom_template
-    open('contents/contact.html', 'w+').write(contact_html)
+    content = open('content/contact.html').read()
+    contact_html = top + content + bottom
+    open('content/contact.html', 'w+').write(contact_html)
 
-    content = open('contents/blog.html').read()
-    blog_html = top_template + content + bottom_template
-    open ('contents/blog.html','w+').write(blog_html)
+    content = open('content/blog.html').read()
+    blog_html = top + content + bottom
+    open ('content/blog.html','w+').write(blog_html)
 
     
     
 
 
 
-if __name__='__main__':
-    main(run_page)    
+if __name__=='__main__':
+    main()    
