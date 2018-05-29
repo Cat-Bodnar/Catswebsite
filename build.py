a673439598f3a5617893f45ecacb5be8bd4c7d24
Cat Bee <catherine.bodnar@gmail.com>
2018-05-28 21:43:09 -0700

    edited build.py

--- build.py
+++ build.py
@@ -4,18 +4,18 @@ def main():
     
     top = open('templates/top.html').read()
     bottom = open('templates/bottom.html').read()
-    content = open('content/index.html').read ()
     
+    content = open('content/index.html').read ()
     index_html = top + content + bottom
-    open('content/index.html','w+').write(index_html)
+    open('docs/index.html','w+').write(index_html)
 
     content = open('content/contact.html').read()
     contact_html = top + content + bottom
-    open('content/contact.html', 'w+').write(contact_html)
+    open('docs/contact.html', 'w+').write(contact_html)
 
     content = open('content/blog.html').read()
     blog_html = top + content + bottom
-    open ('content/blog.html','w+').write(blog_html)
+    open ('docs/blog.html','w+').write(blog_html)
 
     
     
