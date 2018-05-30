bc7a364849e5ee7b343df3985671a6ab5a1c38d9
Cat Bee <catherine.bodnar@gmail.com>
2018-05-29 19:14:36 -0700

    removed docs files to retest main function

--- build.py
+++ build.py
@@ -1,6 +1,23 @@
 def main():
-   
-   pages = [
+   for page in pages: 
+        
+    
+            top = open('templates/top.html').read()
+            bottom = open('templates/bottom.html').read()
+    
+            content = open('content/index.html').read ()
+            index_html = top + content + bottom
+            open('docs/index.html','w+').write(index_html)
+
+            content = open('content/contact.html').read()
+            contact_html = top + content + bottom
+            open('docs/contact.html', 'w+').write(contact_html)
+
+            content = open('content/blog.html').read()
+            blog_html = top + content + bottom
+            open ('docs/blog.html','w+').write(blog_html)
+            
+        pages = [
     {
         'filename': 'content/index.html',
         'title':'Bio',
@@ -14,24 +31,6 @@ def main():
         'title': 'Contact Me',
     }
 ]
-
-    for x in pages: 
-        
-    
-        top = open('templates/top.html').read()
-        bottom = open('templates/bottom.html').read()
-    
-        content = open('content/index.html').read ()
-        index_html = top + content + bottom
-        open('docs/index.html','w+').write(index_html)
-
-        content = open('content/contact.html').read()
-        contact_html = top + content + bottom
-        open('docs/contact.html', 'w+').write(contact_html)
-
-        content = open('content/blog.html').read()
-        blog_html = top + content + bottom
-        open ('docs/blog.html','w+').write(blog_html)
     
     
 if __name__=='__main__':
